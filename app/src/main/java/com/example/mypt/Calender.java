441a45c991153c521ea7677aa41ec451cd3abdf3
bbakssang <alexpark216@gmail.com>
2021-09-16 22:20:44 +0900

    calender 추가

---
+++ app/src/main/java/com/example/mypt/Calender.java
@@ -0,0 +1,14 @@
+package com.example.mypt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+    }
+}
