0b35cb8baea3593148dab2d5e8758b6f25d9c246
Hoon <dbehgns118@naver.com>
2021-09-17 08:46:27 +0900

    마이페이지 before / after 버튼 before / after 라인 이전누를시 사진이 이전화면으로 전환 / 다음은 다음화면으로 전환
    루틴생성 버튼 <xml만생성 아직미구현>
    마이페이지 스크롤view에 imageview로 각각의 button생성 <각각의 버튼에대한 java아직 안짜놈>

---
+++ app/src/main/java/com/example/mypt/BodyPicture.java
@@ -0,0 +1,87 @@
+package com.example.mypt;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BodyPicture extends AppCompatActivity {
+
+    private int CURRENT_INDEX;
+    private ImageView[] imageViews;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bodypicture);
+        init();
+    }
+
+    private void init(){
+
+        ImageView imageView1 = (ImageView)findViewById( R.id.image1);
+        ImageView imageView2 = (ImageView)findViewById( R.id.image2);
+
+        imageViews = new ImageView[]{ imageView1, imageView2};
+        imageView1.setVisibility(View.VISIBLE);
+        CURRENT_INDEX = 0;
+    }
+
+    public void onClickNext(View view){
+
+        if( ++CURRENT_INDEX > 3) {
+            Toast toast = Toast.makeText(BodyPicture.this, "마지막 이미지 입니다.", Toast.LENGTH_SHORT );
+            toast.show();
+
+            CURRENT_INDEX--;
+        }
+        else{
+            for( int i = 0; i < imageViews.length; i++){
+                if( i == CURRENT_INDEX){
+                    imageViews[i].setVisibility(View.VISIBLE);
+                }
+                else{
+                    imageViews[i].setVisibility(View.INVISIBLE);
+                }
+            }
+        }
+    }
+
+    public void onClickPrev(View view){
+
+
+
+        if( --CURRENT_INDEX < 0) {
+            Toast toast = Toast.makeText(BodyPicture.this, "처음 이미지 입니다.", Toast.LENGTH_SHORT );
+            toast.show();
+
+            CURRENT_INDEX++;
+        }
+        else{
+            for( int i = 0; i < imageViews.length; i++){
+                if( i == CURRENT_INDEX){
+                    imageViews[i].setVisibility(View.VISIBLE);
+                }
+                else{
+                    imageViews[i].setVisibility(View.INVISIBLE);
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/mypt/Routine.java
@@ -0,0 +1,14 @@
+package com.example.mypt;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Routine extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routine);
+    }
+}
