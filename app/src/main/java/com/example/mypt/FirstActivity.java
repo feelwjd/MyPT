3357771ddb296346ab613b9b9388c2cc6c5c2a2d
JGHyun <taylor88@naver.com>
2021-09-26 22:15:20 +0900

    20210926_22시15분

--- app/src/main/java/com/example/mypt/FirstActivity.java
+++ app/src/main/java/com/example/mypt/FirstActivity.java
@@ -6,9 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
-
+    private long backKeyPressedTime = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +17,28 @@ public class FirstActivity extends AppCompatActivity {
         //타이틀바 없애는 코드
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-        //3초뒤 Loginactivity로 전환되는 코드
+        //2초뒤 Loginactivity로 전환되는 코드
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(FirstActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
-        }, 3000);
+        }, 2000);
+    }
+    public void onBackPressed() {
+        // 기존의 뒤로가기 버튼의 기능 제거
+        // 2000밀리초 = 2초
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 2초 이내에 뒤로가기 버튼을 한번 더 클릭시 앱 종료
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            finishAffinity();
+            System.runFinalization();
+            System.exit(0);
+        }
     }
 }
