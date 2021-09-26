a5e659532152fbc13031189800569d7991a96093
JGHyun <taylor88@naver.com>
2021-09-26 22:05:56 +0900

    20210926_22시05분

--- app/src/main/java/com/example/mypt/FirstActivity.java
+++ app/src/main/java/com/example/mypt/FirstActivity.java
@@ -3,7 +3,9 @@ package com.example.mypt;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -14,7 +16,13 @@ public class FirstActivity extends AppCompatActivity {
         //타이틀바 없애는 코드
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-
-
+        //3초뒤 Loginactivity로 전환되는 코드
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(FirstActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
     }
 }
