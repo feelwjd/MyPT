7599fb870dc486570707b93d7b65f465a1814db0
feelwjd <feelwjd@gmail.com>
2021-10-04 14:18:49 +0900

    NODE.js API Config.java setting

--- app/src/main/java/com/example/mypt/Config.java
+++ app/src/main/java/com/example/mypt/Config.java
@@ -1,4 +1,23 @@
 package com.example.mypt;
 
 public class Config {
+    public static String BASE = "http://ec2-3-34-96-177.ap-northeast-2.compute.amazonaws.com:3000/";
+    public static String USERS = BASE + "users/";
+    public static String SIGNUP = BASE + "users/signup";
+    public static String SIGNIN = BASE + "users/signin";
+    public static String SIGNOUT = BASE + "users/signout";
+    public static String SIGNDEL = BASE + "users/signdel";
+    public static String MAINPAGE = BASE + "main/";
+    public static String CALENDAR = BASE + "main/calendar";
+    public static String TODO = BASE + "main/todolist";
+    public static String FUNC = BASE + "func/";
+    public static String MKROUTINE = BASE + "func/mkroutine";
+    public static String PICSHARE = BASE + "func/picshare";
+    public static String MYPAGE = BASE + "mypage/";
+    public static String BEFOREAFTER = BASE + "mypage/beforeafter";
+    public static String PRODUCEROUTINE = BASE + "mypage/produce_routine";
+    public static String SETROUTINE = BASE + "mypage/set_routine";
+    public static String COMMU = BASE + "commu/";
+    public static String SHARE = BASE + "commu/share";
+    public static String ILIKEYOU = BASE + "commu/i_like_you";
 }
