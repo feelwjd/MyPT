65bad9374bd68be4866d9c2d100f980a9dbfb80b
feelwjd <feelwjd@gmail.com>
2021-10-23 01:36:16 +0900

    Descript Json Parsing

--- app/src/main/java/com/example/mypt/Config.java
+++ app/src/main/java/com/example/mypt/Config.java
@@ -2,7 +2,7 @@ package com.example.mypt;
 
 public class Config { // URL 형식으로 통신함. 각 요소에 필요한 파라미터 값을 넘겨줘야 작동함. 파라미터 값 유무 잘 확인할 것.
     // 기능이 없어지고 통합되는 부분도 있을 수 있으니 그건 따로 전달해드리겠음.
-    public static String BASE = "http://ec2-3-34-96-177.ap-northeast-2.compute.amazonaws.com:8000/"; //기본 우리 서버주소
+    public static String BASE = "3.34.96.177:8000/"; //기본 우리 서버주소
     public static String USERS = BASE + "users/"; //이거 아무것도아닌데 나중에 추가되는 부분이 있을 수 있음.
     public static String SIGNUP = BASE + "users/signup"; // 회원가입 기능. 파라미터 값으론 userid, pw, username, height, weight, sex 있음 *sex는 0이 남자 / 1이 여자임.
     public static String SIGNIN = BASE + "users/signin"; // 로그인 기능. 파라미터 값으로 userid, pw가 필요.
@@ -29,4 +29,6 @@ public class Config { // URL 형식으로 통신함. 각 요소에 필요한 파
     public static String APIROUTINE_WORKOUT = BASE + "api/routine-workout"; // 루틴별 운동 목록 확인 기능. 보완예정임...
     public static String APIUSER_ROUTINE_WORKOUT = BASE + "api/user-routine-workout"; // 사용자별 루틴 정보 및 루틴내의 운동목록 확인 기능. 파라미터 값으로 userid 필요.
     public static String APIWORKOUT_CLASS = BASE + "api/workout-class"; // 기구 필요 유무 운동 확인 기능. 파라미터 값으로 workout_class 필요. 0은 기구 필요X / 1은 기구 필요O
+    public static String APIROUTINEINFO = BASE + "api/routine-info"; // 루틴의 전반적인 정보를 나타냄. 파라미터 값으로 userid 필요.
+    public static String SHAREIMAGE = BASE + "shareimage/image"; // 사진을 넣으면 루틴정보가 담긴 사진으로 반환해줌. 파라미터 값으로 UserRoutineId 값이 필요. UserRoutineId는 APIROUTINEINFO에서 확인 가능함.
 }
