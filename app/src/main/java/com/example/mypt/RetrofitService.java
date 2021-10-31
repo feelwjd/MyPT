41bbc56315a6a53243c3164935da19605e099bfe
feelwjd <feelwjd@gmail.com>
2021-11-01 03:14:52 +0900

    community use picasso

--- app/src/main/java/com/example/mypt/RetrofitService.java
+++ app/src/main/java/com/example/mypt/RetrofitService.java
@@ -1,6 +1,9 @@
 package com.example.mypt;
 
 import com.example.mypt.api.RoutineInfoVO;
+import com.example.mypt.commu.Community_Data;
+import com.example.mypt.commu.ShareObject;
+import com.example.mypt.commu.shareVO;
 import com.example.mypt.users.SigndelObject;
 import com.example.mypt.users.SigndelVO;
 import com.example.mypt.users.SigninObject;
@@ -13,6 +16,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.HTTP;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
@@ -33,10 +37,6 @@ public interface RetrofitService {
     Call<List<Community_Data>> getCommunity();
 
     @Headers("Content-Type: application/json")
-    @HTTP(method = "DELETE", path = "http://3.34.96.177:8000/users/signdel", hasBody = true)
-    Call<SigndelVO> getSigndel(@Body SigndelObject signdelObject);
-
-    @Headers("Content-Type: application/json")
     @POST("users/signup")
     Call<SignupVO> getSignup(@Body SignupObject signupObject);
 
@@ -44,11 +44,5 @@ public interface RetrofitService {
     @POST("users/signdel")
     Call<SigndelVO> getSigndel(@Body SigndelObject signdelObject);
 
-    @Headers("Content-Type: application/json")
-    @GET("commu/share_show_all")
-
-    Call<List<shareVO>> getCommu();
-    @POST("commu/share")
-    Call<ShareVO> getShare(@Body ShareObject shareObject);
 
 }
