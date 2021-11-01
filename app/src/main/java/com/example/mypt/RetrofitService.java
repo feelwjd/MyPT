68eaa2fc68ab46aa86d85f814ddcdcc4f615b8ce
JGHyun <taylor88@naver.com>
2021-11-01 12:26:53 +0900

    11

--- app/src/main/java/com/example/mypt/RetrofitService.java
+++ app/src/main/java/com/example/mypt/RetrofitService.java
@@ -2,9 +2,8 @@ package com.example.mypt;
 
 import com.example.mypt.api.RoutineInfoVO;
 import com.example.mypt.commu.Community_Data;
-import com.example.mypt.commu.CommunityallObject;
-import com.example.mypt.commu.CommunityallVO;
 import com.example.mypt.commu.ShareObject;
+import com.example.mypt.commu.ShareVO;
 import com.example.mypt.commu.ShareVO;
 import com.example.mypt.users.SigndelObject;
 import com.example.mypt.users.SigndelVO;
@@ -15,13 +14,17 @@ import com.example.mypt.users.SignupVO;
 
 import java.util.List;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.HTTP;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
+import retrofit2.http.Part;
 
 public interface RetrofitService {
 
@@ -37,18 +40,20 @@ public interface RetrofitService {
 
     @Headers("Content-Type: application/json")
     @GET("commu/share_show_all")
-    Call<List<Community_Data>> getCommunity();
-
-    @Headers("Content-Type: application/json")
-    @HTTP(method = "DELETE", path = "http://3.34.96.177:8000/users/signdel", hasBody = true)
-    Call<SigndelVO> getSigndel(@Body SigndelObject signdelObject);
+    Call<List<ShareVO>> getCommu();
 
     @Headers("Content-Type: application/json")
     @POST("users/signup")
     Call<SignupVO> getSignup(@Body SignupObject signupObject);
 
     @Headers("Content-Type: application/json")
-    @POST("commu/share")
-    Call<ShareVO> getShare(@Body ShareObject shareObject);
+    @POST("users/signdel")
+    Call<SigndelVO> getSigndel(@Body SigndelObject signdelObject);
+
+    @Multipart
+    @POST("shareimage/image")
+    Call<ResponseBody> getShare(@Part MultipartBody.Part image, @Part("userid") RequestBody userid ,
+                                @Part("userroutineid") int userroutineid, @Part("commudescript") RequestBody commudescript);
+
 
 }
